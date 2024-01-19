013e11ff810a48d36a1033f9ec912b7d6c67c810
youhyunwoo99 <casshyunwoo@naver.com>
2024-01-19 20:27:14 +0900

    수열과 구간쿼리 2

--- Basic/Basic/수열과 구간쿼리2.cpp
+++ Basic/Basic/수열과 구간쿼리2.cpp
@@ -5,13 +5,24 @@ using namespace std;
 
 vector<int> solution(vector<int> arr, vector<vector<int>> queries) {
     vector<int> answer;
-    for (int i = 0; i < queries.size(); ++i) {
+    for(int i = 0; i<queries.size(); i++)
+    {
         int k = 1000001;
-        for (int j = queries[i][0]; j <= queries[i][1]; ++j) {
-            if (arr[j] > queries[i][2] && arr[j] < k) k = arr[j];
+        for(int j = queries[i][0]; j<=queries[i][1]; j++)
+        {
+            if(arr[j] > queries[i][2] && k>arr[j])
+            {
+                k=arr[j];
+            }
         }
-        if (k == 1000001) answer.emplace_back(-1);
-        else answer.emplace_back(k);
+        if(k==1000001)
+        {
+            answer.push_back(-1);
+        }
+        else
+        {
+            answer.push_back(k);
+        }
     }
     return answer;
 }
