c5211174f3d0ba5f1723c2dc5aee708385667d29
youhyunwoo99 <casshyunwoo@naver.com>
2024-01-23 20:05:59 +0900

    콜라츠 수열 만들기

---
+++ Basic/Basic/콜라츠 수열 만들기.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n) {
+    vector<int> answer;
+    answer.push_back(n);
+    while (n != 1)
+    {
+        if (n % 2 == 0)
+        {
+            n = n / 2;
+        }
+        else if (n % 2 == 1)
+        {
+            n = 3 * n + 1;
+        }
+        answer.push_back(n);
+    }
+
+    return answer;
+}
