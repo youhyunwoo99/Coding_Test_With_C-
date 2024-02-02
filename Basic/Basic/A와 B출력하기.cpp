03a714c0f01508233a86bdd091dbc1dd475e0a13
youhyunwoo99 <casshyunwoo@naver.com>
2024-02-02 13:27:12 +0900

    A와 B출력하기

---
+++ Basic/Basic/A와 B출력하기.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int a=0;
+    int b=0;
+    cin >> a >> b;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    return 0;
+}
