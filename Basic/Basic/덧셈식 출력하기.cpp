943b87bb7b2c1d248f58b75b1eeaf9c74797b23f
youhyunwoo99 <casshyunwoo@naver.com>
2024-02-07 10:35:54 +0900

    덧셈식 출력하기

---
+++ Basic/Basic/덧셈식 출력하기.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int a;
+    int b;
+    int c;
+    cin >> a >> b;
+    c = a+b;
+    cout << a << " + " << b << " = " << c << endl;
+    return 0;
+}
