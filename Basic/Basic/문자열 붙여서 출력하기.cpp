ce131c775f12e5cf25252df8b60949dfdd617df8
youhyunwoo99 <casshyunwoo@naver.com>
2024-02-08 20:27:01 +0900

    문자열 붙여서 출력하기

---
+++ Basic/Basic/문자열 붙여서 출력하기.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str1, str2, str3;
+    cin >> str1 >> str2;
+    str3 = str1+str2;
+    cout << str3 << endl;
+    return 0;
+}
