fa1f1e4f7f9beb7a406c05f3396dc3759e0045d3
youhyunwoo99 <casshyunwoo@naver.com>
2024-02-10 10:45:21 +0900

    홀짝 구분하기

---
+++ Basic/Basic/홀짝 구분하기.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int n;
+    cin >> n;
+    if(n % 2 == 0)
+    {
+        cout << n << " is even" << endl;
+    }
+    else
+    {
+        cout << n << " is odd" << endl;
+    }
+    return 0;
+}
