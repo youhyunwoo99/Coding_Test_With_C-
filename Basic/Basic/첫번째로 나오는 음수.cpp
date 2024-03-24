79f5b75e62335bfbb6650323dfd466afa30ff331
youhyunwoo99 <casshyunwoo@naver.com>
2024-03-25 08:55:15 +0900

    [ADD] 첫번째로 나오는 음수

---
+++ Basic/Basic/첫번째로 나오는 음수.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int answer = 0;
+    for(int i = 0; i<num_list.size(); i++)
+    {
+        if(num_list[i] < 0)
+        {
+            answer = i;
+            break;
+        }
+        answer = -1;
+    }
+    return answer;
+}
