c13815178b8ccdca3eb2a4c561761ae2b10af03b
youhyunwoo99 <casshyunwoo@naver.com>
2024-05-23 10:58:16 +0900

    [ADD] A 강조하기

---
+++ Basic/Basic/A 강조하기.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) {
+    for(int i = 0; i<myString.size(); i++)
+    {
+        if(myString[i] == 'a')
+        {
+            myString[i] = toupper(myString[i]);
+        }
+        else if(myString[i] != 'A')
+        {
+            myString[i] = tolower(myString[i]);
+        }
+    }
+    return myString;
+}
