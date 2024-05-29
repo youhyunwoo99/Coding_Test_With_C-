0439ef8f366ac27d80c14f2f0beb702483937c9f
youhyunwoo99 <casshyunwoo@naver.com>
2024-05-29 23:28:51 +0900

    [ADD] 공백으로 구분하기 1

---
+++ Basic/Basic/공백으로 구분하기 1.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<string> solution(string my_string) {
+    vector<string> answer;
+    string temp;
+    
+    for(int i = 0; i<my_string.size(); i++)
+    {
+        if(my_string[i] != ' ')
+        {
+            temp += my_string[i];
+        }
+        else
+        {
+            answer.push_back(temp);
+            temp = "";
+        }
+    }
+    answer.push_back(temp);
+    return answer;
+}
